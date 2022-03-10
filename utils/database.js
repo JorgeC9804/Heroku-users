fccfb968f8cb969bd4dc238437e39d985af72eae
Jorge Castro <jorgs705@gmail.com>
2022-03-09 18:06:57 -0600

    init heroku

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,32 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
+/**
+ * path lo mandamos directo a la raiz
+ */
 const sequelize = new Sequelize({
-	host: 'localhost',
-	username: 'postgres',
-	password: '1998',
-	database: 'relations-example',
-	dialect: 'postgres',
-	logging: false
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  database: process.env.DB,
+  dialect: "postgres",
+  logging: false,
+  // parte de Heroku que neceista sequelize para conectarse a produccion
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
+
+/* Host ec2-44-193-188-118.compute-1.amazonaws.com
+Database d814jr3li1ujoj
+User ruostyfequbjix
+Port 5432
+Password 6ee6b48f365e3f841ff70d8bb6b24d30053e7021796395de6fab41cf64ae45b1
+*/
 
 module.exports = { sequelize };
